3b5fcc30931e9f5cd4a34637a0073a570a092347
keni2002 <yerbisuniverses@gmail.com>
2023-12-20 08:48:39 -0500

    add: el resto de las operaciones CRUD. update, delete,

--- src/controller/tasksController.js
+++ src/controller/tasksController.js
@@ -1,3 +1,5 @@
+const { where } = require('sequelize');
+
 const Users = require('../database').Users;
 const Tasks = require('../database').Tasks;
 
@@ -5,6 +7,46 @@ exports.getTasksController = async (req, res) => {
     const id = req.user.id;
     const listTasks = await Tasks.findAll({ where: { userId: id } })
     res.json(listTasks)
+}
+exports.getOneController = async (req, res) => {
+    const id = req.params.id;
+    const task = await Tasks.findOne({ where: { userId: id  } });
+    res.json(task);
+}
+exports.updateTaskController = async (req, res) => {
+    const id = req.params.id;
+    const task = await Tasks.findOne({ where: { userId: id  } });
+    if(!task) {
+        return res.status(400).json({message: "task not found"})
+    }
+    const { title, content, targetdate } = req.body;
+    
+    if (!title || !content || !targetdate) {
+        return res.status(400).json({ message: "Something Empty" })
+    }
+    const ok = await Tasks.update({ title, content, targetdate },
+        {
+            where: { userId: id }
+        })
+    if(ok) {
+        return res.json({message: "updated successfull"})
+    }
+    
+}
+exports.deleteTaskController = async (req, res) => {
+    const id = req.params.id;
+    const task = await Tasks.findOne({ where: { userId: id  } });
+    if(!task) {
+        return res.status(400).json({message: "task not found"})
+    }
+    const ok = await Tasks.destroy({
+        where: {
+            where: { userId: id }
+        }
+    })
+    if(ok) {
+        return res.json({message: "deleted successfull"})
+    }
 }
 exports.addTasksController = async (req, res) => {
     const userId = req.user.id;
@@ -19,7 +61,7 @@ exports.addTasksController = async (req, res) => {
             return res.status(500).json({ error: "Cannot register user at the moment!" });
         }
     )
-    if (saveTask){
+    if (saveTask) {
         return res.json({ message: "Task added successfully" });
     }
 }
